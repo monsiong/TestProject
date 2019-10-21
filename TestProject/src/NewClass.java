2aa01e26712672acd342ca2a2cd80f5d2d8e9047
Owner1 <Owner1@DESKTOP-33EQUJ0>
2019-10-21 15:40:42 +0800

    Change Default 2 2

--- TestProject/src/NewClass.java
+++ TestProject/src/NewClass.java
@@ -4,6 +4,9 @@ public class NewClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.print("Test");
+			System.out.print("Test2");
+			System.out.print("Test3");
+			System.out.print("Test4");
 	}
 
 }
