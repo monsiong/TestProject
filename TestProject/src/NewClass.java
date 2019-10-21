23ae50dc778c9e55bed5fb5cf16a582ebaf10833
Owner <Owner@DESKTOP-33EQUJ0>
2019-10-21 14:57:59 +0800

    Change default value

--- TestProject/src/NewClass.java
+++ TestProject/src/NewClass.java
@@ -3,7 +3,9 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.print("Test");
+		    System.out.print("Test 12");
+			System.out.print("Test 123");
+			System.out.print("Test 1234");
 	}
 
 }
